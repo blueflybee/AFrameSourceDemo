80dde159d1da72a0f3e9b1ee9348294b78ff818e
shaojun <wusj2017@gmail.com>
2019-08-06 14:12:02 +0800

    color state list测试

--- app/src/main/java/myorg/aframe/aframesourcedemo/MainActivity.java
+++ app/src/main/java/myorg/aframe/aframesourcedemo/MainActivity.java
@@ -1,5 +1,13 @@
 package myorg.aframe.aframesourcedemo;
 
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteCursor;
+import android.database.sqlite.SQLiteCursorDriver;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQuery;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    SQLiteDatabase database = openOrCreateDatabase("test", MODE_ENABLE_WRITE_AHEAD_LOGGING, null, new DatabaseErrorHandler() {
+      @Override
+      public void onCorruption(SQLiteDatabase sqLiteDatabase) {
+
+      }
+    });
+
+    registerComponentCallbacks(new ComponentCallbacks() {
+      @Override
+      public void onConfigurationChanged(Configuration configuration) {
+
+      }
+
+      @Override
+      public void onLowMemory() {
+
+      }
+    });
+
   }
 }
