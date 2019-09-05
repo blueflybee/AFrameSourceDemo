9988718ebe14c58d2d8f1612a07c042cebb92064
shaojun <wusj2017@gmail.com>
2019-09-05 10:50:53 +0800

    LiveData学习

--- app/src/main/java/myorg/aframe/aframesourcedemo/DataBindingActivity.java
+++ app/src/main/java/myorg/aframe/aframesourcedemo/DataBindingActivity.java
@@ -1,12 +1,19 @@
 package myorg.aframe.aframesourcedemo;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.util.Function;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.Transformations;
 import myorg.aframe.aframesourcedemo.databinding.ActivityDataBindingBinding;
 import myorg.aframe.aframesourcedemo.viewmodel.MyViewModel;
 
@@ -15,11 +22,48 @@ public class DataBindingActivity extends AppCompatActivity {
 
   private ActivityDataBindingBinding mBinding;
   private MyViewModel mMyViewModel;
+  private MutableLiveData<Integer> mSource;
+  private MutableLiveData<Integer> mLiveData1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     mBinding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding);
+
+    testTransformationsMap();
+    testTransformationsSwitchMap();
+
+//    MediatorLiveData<Integer> mediatorLiveData = new MediatorLiveData<>();
+//    MutableLiveData<Integer> liveData1 = new MutableLiveData<>();
+//    MutableLiveData<Integer> liveData2 = new MutableLiveData<>();
+
+//    mediatorLiveData.observe(this, new Observer<Integer>() {
+//      @Override
+//      public void onChanged(Integer integer) {
+//        System.out.println("DataBindingActivity.mediatorLiveData.onChanged");
+//      }
+//    });
+//
+//
+//    mediatorLiveData.addSource(liveData1, new Observer<Integer>() {
+//      @Override
+//      public void onChanged(Integer integer) {
+//        System.out.println("DataBindingActivity.liveData1.integer = " + integer);
+////        mediatorLiveData.setValue(integer);
+//      }
+//    });
+//
+//    mediatorLiveData.addSource(liveData2, new Observer<Integer>() {
+//      @Override
+//      public void onChanged(Integer integer) {
+//        System.out.println("DataBindingActivity.liveData2.integer = " + integer);
+////        mediatorLiveData.setValue(integer);
+//      }
+//    });
+//
+//
+//    liveData1.setValue(1);
+
 
     mMyViewModel = new MyViewModel();
 
@@ -27,21 +71,33 @@ public class DataBindingActivity extends AppCompatActivity {
     mBinding.setViewModel(mMyViewModel);
 
 
-
     mMyViewModel.getName().setValue("abcd");
 
     mMyViewModel.getTime().setValue("start");
 
     final int[] i = {1};
+    final int[] j = {100};
     mBinding.etTest.setOnClickListener(new View.OnClickListener() {
 
       @Override
       public void onClick(View view) {
         System.out.println("DataBindingActivity.onClick");
 
-        mMyViewModel.getTime().postValue(String.valueOf(i[0]++));
+        mMyViewModel.getTime().setValue(String.valueOf(i[0]++));
 
 
+      }
+    });
+
+
+    mBinding.btn2.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        System.out.println("DataBindingActivity.before modify time = " + mMyViewModel.getTime().getValue());
+        mBinding.myBindview.setTime(String.valueOf(j[0]++));
+        mBinding.myBindview.setClickable(true);
+//        mBinding.myBindview.performClick();
+        System.out.println("DataBindingActivity.after modify time = " + mMyViewModel.getTime().getValue());
       }
     });
 
@@ -64,6 +120,40 @@ public class DataBindingActivity extends AppCompatActivity {
 
       }
     });
+
+    mBinding.btn3.setOnClickListener(new View.OnClickListener() {
+      int i = 1;
+      int j = -1;
+
+      @Override
+      public void onClick(View view) {
+//        mSource.setValue(i++);
+        mLiveData1.setValue(j--);
+      }
+    });
+  }
+
+  private void testTransformationsSwitchMap() {
+
+    mLiveData1 = new MutableLiveData<>();
+    Transformations
+        .switchMap(mLiveData1, input -> {
+          MutableLiveData<String> liveDataY = new MutableLiveData<>();
+          liveDataY.setValue("you input: " + input);
+          return liveDataY;
+        })
+        .observe(this, s -> System.out.println("result = " + s));
+
+  }
+
+  private void testTransformationsMap() {
+
+//    mSource = new MutableLiveData<>();
+//    Transformations
+//        .map(mSource, input -> "you input: " + input)
+//        .observe(this, result -> System.out.println("result = " + result));
+
+
   }
 
 }
--- app/src/main/java/myorg/aframe/aframesourcedemo/adapter/MyViewBindingAdapter.java
+++ app/src/main/java/myorg/aframe/aframesourcedemo/adapter/MyViewBindingAdapter.java
@@ -4,28 +4,45 @@ import android.view.View;
 
 import androidx.databinding.BindingAdapter;
 import androidx.databinding.InverseBindingAdapter;
+import androidx.databinding.InverseBindingListener;
 import myorg.aframe.aframesourcedemo.customview.MyBindView;
 
 public class MyViewBindingAdapter {
 
   @BindingAdapter("time")
   public static void setTime(MyBindView view, String  time) {
-    System.out.println("MyViewBindingAdapter.setTime");
+
     String oldTime = view.getTime();
     if (!oldTime.equals(time)) {
+      System.out.println("MyViewBindingAdapter.setTime");
       view.setTime(time);
-      view.postInvalidateDelayed(200);
+      view.postInvalidate();
     }
 
   }
 
-  @InverseBindingAdapter(attribute = "time")
+  @BindingAdapter(value = "app:timeAttrClickChanged", requireAll = false)
+  public static void setOnClickListener(MyBindView view, final InverseBindingListener inverseBindingListener) {
+//    if (inverseBindingListener != null) {
+//      view.setTime("app:timeAttrClickChanged");
+//      inverseBindingListener.onChange();
+//    }
+    view.setOnClickListener(view1 -> {
+      if (inverseBindingListener != null) {
+        System.out.println("MyViewBindingAdapter.setOnClickListener");
+        inverseBindingListener.onChange();
+      }
+    });
+
+  }
+
+  @InverseBindingAdapter(attribute = "time", event = "app:timeAttrClickChanged")
   public static String getTime(MyBindView view) {
     return view.getTime();
   }
 
 
-  @BindingAdapter("app:hideIfZero")
+  @BindingAdapter("hideIfZero")
   public static void hideIfZero(View view, int number) {
     System.out.println("MyViewBindingAdapter.hideIfZero");
     view.setVisibility(number == 0 ? View.GONE : View.VISIBLE);
